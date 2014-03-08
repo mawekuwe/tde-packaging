d9c948c7660fd487f81da3e864cd1e7e7811649b
François Andriot <albator78@libertysurf.fr>
2014-03-08 10:36:59 +0100

    RPM packaging: update build script

--- redhat/build/get_specfile.sh
+++ redhat/build/get_specfile.sh
@@ -5,23 +5,23 @@ TDE_VERSION="${2:-14.0.0}"
 
 [ -z "${PKGNAME}" ] && echo "You must specify a package name !" && exit 1
 
-[ -d "${TDE_SPECDIR}" ] || TDE_SPECDIR=~/tde/tde-packaging/redhat
+[ -d "${PACKAGING_DIR}" ] || PACKAGING_DIR=~/tde/tde-packaging/redhat
 
 # Special case for QT3
 if [ "${PKGNAME}" = "qt3" ]; then
   case "$(rpmdist.sh --dist)" in
-    .oss*) TDE_SPECDIR=~/tde/tde-packaging/opensuse ;;
-    .mga*|.mdk*|.pclos*) TDE_SPECDIR=~/tde/tde-packaging/mageia ;;
+    .oss*) PACKAGING_DIR=~/tde/tde-packaging/opensuse ;;
+    .mga*|.mdk*|.pclos*) PACKAGING_DIR=~/tde/tde-packaging/mageia ;;
   esac
 fi
 
-SPECFILE=$(find "${TDE_SPECDIR}" -name "${PKGNAME}-${TDE_VERSION}.spec")
+SPECFILE=$(find "${PACKAGING_DIR}" -name "${PKGNAME}-${TDE_VERSION}.spec")
 if [ ! -r "${SPECFILE}" ]; then
-  SPECFILE=$(find "${TDE_SPECDIR}" -name "${PKGNAME}.spec")
+  SPECFILE=$(find "${PACKAGING_DIR}" -name "${PKGNAME}.spec")
   if [ ! -r "${SPECFILE}" ]; then
-    SPECFILE=$(find "${TDE_SPECDIR}" -name "trinity-${PKGNAME}-${TDE_VERSION}.spec")
+    SPECFILE=$(find "${PACKAGING_DIR}" -name "trinity-${PKGNAME}-${TDE_VERSION}.spec")
     if [ ! -r "${SPECFILE}" ]; then
-      SPECFILE=$(find "${TDE_SPECDIR}" -name "trinity-${PKGNAME}.spec")
+      SPECFILE=$(find "${PACKAGING_DIR}" -name "trinity-${PKGNAME}.spec")
     fi
   fi
 fi
