817b99fec21b7d89d20d3f45fc5d7712e8af09ad
François Andriot <albator78@libertysurf.fr>
2014-10-05 13:18:09 +0200

    RPM Packaging: update build script

--- redhat/build/get_rpm_package_name.sh
+++ redhat/build/get_rpm_package_name.sh
@@ -17,23 +17,24 @@ DEVEL="$2"
 
 # Some runtime packages are prefixed with 'trinity-', some are not.
 case "${PKGNAME}" in
-  "trinity-"*|"qt3"|"tqt3"|"tqtinterface"|"python-qt3"|"esound"|*"-tqt"|"lib"*) PREFIX="";;
+  "trinity-"*|"qt3"|"tqt3"|"tqtinterface"|"python-qt3"|"esound"|"avahi-tqt"|"dbus-tqt"|"dbus-1-tqt"|"libart-lgpl"|"fileshareset") PREFIX="";;
   "autoconf"|"automake"|"cmake"|"gnuchess"|"htdig"|"imlib1"|"libotr3"|"libtool"|"lilypond"|"m4"|"mftrace"|"pcsc-perl"|"torsocks"|"wv2") PREFIX="";;
   "curl") PREFIX="trinity-lib";;
   *) PREFIX="trinity-";;
 esac
 
 # Runtime packages
+lib=$(rpm -E %_lib)
 case "${PKGNAME}" in
   # Some packages have different runtime name than source package.
-  "avahi-tqt") PKGRUNTIME="libavahi-tqt1";;
-  "dbus-tqt") PKGRUNTIME="libdbus-tqt-1-0";;
-  "dbus-1-tqt") PKGRUNTIME="libdbus-1-tqt0";;
+  "avahi-tqt") PKGRUNTIME="${lib}avahi-tqt1";;
+  "dbus-tqt") PKGRUNTIME="${lib}dbus-tqt-1-0";;
+  "dbus-1-tqt") PKGRUNTIME="${lib}dbus-1-tqt0";;
   "esound") PKGRUNTIME="esound-libs";;
   "koffice") PKGRUNTIME="trinity-koffice-suite";;
-  "libart-lgpl") PKGRUNTIME="libart_lgpl_2-2";;
-  "tqt3") PKGRUNTIME="libtqt3-mt";;
-  "tqtinterface") PKGRUNTIME="libtqt4";;
+  "libart-lgpl") PKGRUNTIME="${lib}art_lgpl_2-2";;
+  "tqt3") PKGRUNTIME="${lib}tqt3-mt";;
+  "tqtinterface") PKGRUNTIME="${lib}tqt4";;
   # Language package: install only French language package
   "k3b-i18n"|"koffice-i18n"|"tde-i18n") PKGRUNTIME="${PKGNAME}-French";;
   # Default case: runtime package has same name as source package
@@ -61,18 +62,17 @@ if [ -n "${DEVEL}" ]; then
 
   # Some package have specific development package.
   case "${PKGNAME}" in
-    "avahi-tqt") PKGRUNTIME="libavahi-tqt-devel";;
-    "dbus-tqt") PKGRUNTIME="libdbus-tqt-1-devel";;
+    "avahi-tqt") PKGDEVEL="libavahi-tqt-devel";;
+    "dbus-tqt") PKGDEVEL="libdbus-tqt-1-devel";;
     "dbus-1-tqt") PKGDEVEL="libdbus-1-tqt-devel";;
     "esound") PKGDEVEL="esound-devel";;
     "libart-lgpl") PKGDEVEL="libart_lgpl-devel";;
-    "tqt3") PKGDEVEL="tqt3-dev-tools";;
+    "tqt3") PKGDEVEL="tqt3-dev-tools tqt3-apps-devel";;
     # Default case: development package has same name as runtime package, plus '-devel' suffix.
     *) PKGDEVEL="${PKGRUNTIME}-devel";;
   esac
 
   # Finally, other packages do have a '-devel'
   echo "${PREFIX}${PKGDEVEL}"
-  
 fi
 
